237299eb0f9301b8700c7d639840f6a1b6cfc4b2
EnzzoAndreas <enzzo.andreascr@gmail.com>
2023-01-20 21:12:08 -0300

    adiciona adção e subtração

--- Main.py
+++ Main.py
@@ -1,15 +1,20 @@
 if __name__ == "__main__":
     
     opcao = -1
-    while opcao != 2:
-        print('calculadora GIT')
+    while opcao != 3:
+        print('\ncalculadora GIT')
         print('=' * 20)
         print('1 - Somar')
-        print('2 - Sair')
+        print('2 - Subtração')
+        print('3 - Sair')
         print('=' * 20)
         opcao = int(input('Opção: '))
 
         if opcao == 1:
             print('Opção Somar')
         elif opcao == 2:
+            print('Opção Subtrair')
+        elif opcao == 3:
             print('Obrigado por usar nossa Calculadora GIT')
+        else:
+            print('Opção INVALIDA, por favor tente novamente')
