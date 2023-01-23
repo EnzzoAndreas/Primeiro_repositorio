1c275843dfce5a4dfa001cc1093a996570e5df61
EnzzoAndreas <enzzo.andreascr@gmail.com>
2023-01-23 18:24:46 -0300

    Branch de Multiplicação criada e atualização do Main

--- Calculadora/operacoes_simples.py
+++ Calculadora/operacoes_simples.py
@@ -3,3 +3,6 @@ def somar(operador1, operador2):
 
 def subtrair(operador1, operador2):
     return operador1 - operador2
+
+def multiplicar(operador1, operador2):
+    return operador1 * operador2
--- Main.py
+++ Main.py
@@ -1,15 +1,16 @@
 
-from Calculadora.operacoes_simples import somar, subtrair
+from Calculadora.operacoes_simples import somar, subtrair, multiplicar
 
 if __name__ == "__main__":
     
     opcao = -1
-    while opcao != 3:
+    while opcao != 4:
         print('\ncalculadora GIT')
         print('=' * 20)
         print('1 - Somar')
         print('2 - Subtração')
-        print('3 - Sair')
+        print('Multiplicação')
+        print('4 - Sair')
         print('=' * 20)
         opcao = int(input('Opção: '))
 
@@ -25,6 +26,11 @@ if __name__ == "__main__":
             resultado = subtrair(operador1, operador2)
             print(f'Resultado: {resultado}')
         elif opcao == 3:
+            operador1 = int(input('Operador 1: '))
+            operador2 = int(input('Operador 2: '))
+            resultado = multiplicar(operador1, operador2)
+            print(f'Resultado: {resultado}')
+        elif opcao == 4:
             print('Obrigado por usar nossa Calculadora GIT')
         else:
             print('Opção INVALIDA, por favor tente novamente')
